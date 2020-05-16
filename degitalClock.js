0447c60ccc89acb07203e062967c55a4dd7c105b
Sagar <57942799+poudel-sagar@users.noreply.github.com>
2020-05-16 19:28:02 +0545

    Create degitalClock.js

---
+++ degitalClock.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>digital clock</title>
+    </head>
+    <body>
+        <div id="MyClockDisplay"></div>
+        <script type="text/javascript">
+        function showTime(){
+            var date=new Date();
+            var h =date.getHours();
+            var m=date.getMinutes();
+            var s=date.getSeconds();
+            var sesson="AM";
+            if(h==0){
+                h=12;
+            }
+            if(h>12){
+                h=h-12;
+                sesson="PM";
+            }
+           h=(h<10)?"0"+h:h;
+           m=(m<10)?"0"+m:m;
+           s=(s<10)?"0"+s:s;
+            var time = h +":"+m+":"+s;
+            document.getElementById("MyClockDisplay").innerText=time;
+            document.getElementById("MyClockDisplay").textContent=time;
+            setTimeout(showTime,1000);
+        }
+        showTime();
+        setInterval(showTime,1000);
+        </script>
+    </body>
+</html>
